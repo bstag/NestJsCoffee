590c024a954144c0ccf350ee6235924f2020632c
Jason Henriksen <blackstag@gmail.com>
2020-09-04 12:04:24 -0500

    Added hard coded connection in root module for postgres.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 
+//Move to config file in the long run as if this was a prod system
+
 @Module({
-  imports: [CoffeesModule],
-  controllers: [AppController ],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'neverusedinproduction',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true, // disable in production and move to migrations.
+    }),
+  ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
