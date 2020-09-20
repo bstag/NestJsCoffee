7fa58dc1b50b327a3477478e47535687f8591471
Jason Henriksen <blackstag@gmail.com>
2020-09-19 21:09:49 -0500

    basic dynamic module use.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { DatabaseModule } from './database/database.module';
 
 //Move to config file in the long run as if this was a prod system
 
@@ -21,6 +22,7 @@ import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
       synchronize: true, // disable in production and move to migrations.
     }),
     CoffeeRatingModule,
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
